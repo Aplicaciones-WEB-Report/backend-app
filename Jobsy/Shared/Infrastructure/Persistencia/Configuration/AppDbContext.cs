3834ba1d3298718398350ce7cdbc4e6966c99839
Cameron Charllote <cameronbustamante2@gmail.com>
2025-06-21 15:26:41 -0500

    feat(message): agrega entidad mensajes, y controler

--- Jobsy/Shared/Infrastructure/Persistencia/Configuration/AppDbContext.cs
+++ Jobsy/Shared/Infrastructure/Persistencia/Configuration/AppDbContext.cs
@@ -11,7 +11,7 @@ public class AppDbContext : DbContext
     }
     public DbSet<User> Usuarios { get; set; }
     public DbSet<Profile> Perfiles { get; set; }
-    
+    public DbSet<Messages> Messages { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         // Relación uno a uno entre User y Profile
