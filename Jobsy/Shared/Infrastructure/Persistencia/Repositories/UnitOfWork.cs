9f9afa8ff4ab8a4655cbe2cd8757e89d037b9b2a
Gabriel Lapa <glapa8557@gmail.com>
2025-06-21 02:11:43 -0500

    feat update Shared

---
+++ Jobsy/Shared/Infrastructure/Persistencia/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+using Jobsy.Shared.Domain.Repositories;
+using Jobsy.Shared.Infrastructure.Persistencia.Configuration;
+
+namespace Jobsy.Shared.Infrastructure.Persistencia.Repositories;
+
+/// <summary>
+///     Unit of work implementation
+/// </summary>
+/// <remarks>
+///     This class implements the basic operations for a unit of work.
+///     It requires the context to be passed in the constructor.
+/// </remarks>
+/// <see cref="IUnitOfWork" />
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    /// <inheritdoc />
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
